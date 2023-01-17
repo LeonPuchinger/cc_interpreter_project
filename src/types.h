0a4dd86a076c49b58818aaaad98285fb01483bb1
LeonPuchinger <lepu1590@gmail.com>
2023-01-17 22:52:14 +0100

    allow easy creation of AST nodes

--- src/types.h
+++ src/types.h
@@ -1,8 +1,10 @@
 #ifndef _TYPES_H
 #define _TYPES_H
 
+#include <stdlib.h>
+
 typedef enum {
-    ND_ROOT
+    ND_ROOT, ND_INT, ND_STR
 } AST_Node_Type;
 
 typedef struct _AST_Node {
@@ -13,5 +15,9 @@ typedef struct _AST_Node {
     };
     struct _AST_Node *children;
 } AST_Node;
+
+AST_Node *empty_node(AST_Node_Type type);
+AST_Node *int_node(int value);
+AST_Node *str_node(char *value);
 
 #endif
