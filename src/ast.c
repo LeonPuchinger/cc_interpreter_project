f7dc97d5e3352d845983496672d2989964c03077
LeonPuchinger <lepu1590@gmail.com>
2023-01-19 00:04:08 +0100

    document AST functions

--- src/ast.c
+++ src/ast.c
@@ -4,13 +4,14 @@
 #define AST_CHILDREN_BUFFER_SIZE 10
 
 void init_children(AST_Node *node) {
-    // initialize AST_Node children list for `node`
+    // Initialize AST_Node children list for `node`
     node->children_size = 0;
     node->children_buffer_size = AST_CHILDREN_BUFFER_SIZE;
     node->children = calloc(AST_CHILDREN_BUFFER_SIZE, sizeof(AST_Node *));
 }
 
 void add_child(AST_Node *node, AST_Node *child) {
+    // Add a child to `node`. If there is not enough space, reallocate more space.
     if (node->children_size >= node->children_buffer_size) {
         node->children_buffer_size += AST_CHILDREN_BUFFER_SIZE;
         node->children = realloc(node->children, node->children_buffer_size);
@@ -20,6 +21,8 @@ void add_child(AST_Node *node, AST_Node *child) {
 }
 
 AST_Node *empty_node(AST_Node_Type type) {
+    // Create a AST node with only a AST node type.
+    // The node does not yet have any value or children.
     AST_Node *new = calloc(1, sizeof(AST_Node));
     new->type = type;
     init_children(new);
@@ -27,12 +30,14 @@ AST_Node *empty_node(AST_Node_Type type) {
 }
 
 AST_Node *int_node(int value) {
+    // Shortcut to initialize a node of type ND_INT with a value.
     AST_Node *new = empty_node(ND_INT);
     new->int_value = value;
     return new;
 }
 
 AST_Node *str_node(char *value) {
+    // Shortcut to initialize a node of type ND_STR with a value.
     AST_Node *new = empty_node(ND_STR);
     new->str_value = value;
     return new;
